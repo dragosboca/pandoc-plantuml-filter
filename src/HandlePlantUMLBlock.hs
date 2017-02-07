f47e6569437bcc9b24922524f1b072c8eb2326ef
Dragos Boca <dragos.boca@gmail.com>
2017-02-07 21:37:11 +0200

    change image format by target format

--- src/HandlePlantUMLBlock.hs
+++ src/HandlePlantUMLBlock.hs
@@ -2,40 +2,51 @@ import Text.Pandoc.JSON
 import Data.ByteString.Lazy (hGetContents, hPut)
 import Data.ByteString.Lazy.UTF8 (fromString)
 import Data.Digest.Pure.SHA (sha1, showDigest)
-import System.IO (hClose, hPutStr, IOMode(..), openBinaryFile)
+import System.IO
+       (hClose, hPutStr, IOMode(..), openBinaryFile, Handle)
 import System.Process
 
-processBlocks :: Block -> IO Block
-processBlocks b =
-  case b of
-    CodeBlock (_ , ["plantuml"], _) content -> plantUMLToImg content
-    _ -> return b
+processBlocks :: Maybe Format -> Block -> IO Block
+processBlocks format cb@(CodeBlock (_, classes, _) contents) =
+  if elem "plantuml" classes || elem "{plantuml}" classes -- stupid - only because of atom markdown-preview-enhanced!!
+    then plantUMLToImg format contents
+    else return cb
+processBlocks _ cb = return cb
 
-plantUMLToImg :: String -> IO Block
-plantUMLToImg content =  do
-  path <- renderImage content
-  return $ Para [Image [] (path, "")]
+plantUMLToImg :: Maybe Format -> String -> IO Block
+plantUMLToImg format content = do
+  path <- renderImage format content
+  return $ Para [Image nullAttr [] (path, "")]
 
-renderImage :: String -> IO String
-renderImage content = do
-  let path = uniqueName content ++ ".eps"
+renderImage :: Maybe Format -> String -> IO String
+renderImage format content = do
+  let path = uniqueName content ++ "." ++ imgFormat format
   (Just hIn, Just hOut, _, _) <-
-    createProcess (proc "plantuml" ["-pipe", "-teps"]){ std_in = CreatePipe,
-                                                        std_out = CreatePipe }
+    createProcess
+      (proc "/bin/sh" ["plantuml", "-pipe", "-t" ++ imgFormat format]) -- shell won't work; I don't know why
+      { std_in = CreatePipe
+      , std_out = CreatePipe
+      }
   hPutStr hIn content
   hClose hIn
-
-  hFile <- openBinaryFile path WriteMode
-  img <- hGetContents hOut
-  hPut hFile img
-
-  hClose hFile
+  writeImageFile hOut path
   hClose hOut
-
   return path
-
-uniqueName :: String -> String
-uniqueName = showDigest . sha1 . fromString
+  where
+    imgFormat :: Maybe Format -> String
+    imgFormat (Just (Format s))
+      | s `elem` ["pdf", "latex"] = "latex"
+      | s `elem` ["html", "markdown"] = "svg"
+      | otherwise = "png"
+    imgFormat _ = "png"
+    uniqueName :: String -> String
+    uniqueName = showDigest . sha1 . fromString
+    writeImageFile :: Handle -> String -> IO ()
+    writeImageFile hOut path = do
+      hFile <- openBinaryFile path WriteMode
+      img <- hGetContents hOut
+      hPut hFile img
+      hClose hFile
 
 main :: IO ()
 main = toJSONFilter processBlocks
